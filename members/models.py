07ea9eaa1378da83eaa286b38494165ca3545972
Rianson <164814917+Rian16Rian@users.noreply.github.com>
2025-03-11 15:54:20 +0800

    changed models code

--- members/models.py
+++ members/models.py
@@ -4,6 +4,4 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-        
-    def __str__(self):
-        return self.title
+    
